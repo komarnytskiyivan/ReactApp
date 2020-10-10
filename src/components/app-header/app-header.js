9a4510f6ecc3ac3045de697b371f84856652a148
Ivan <totalcommander100@gmail.com>
2020-10-10 13:39:41 +0300

    Added many components and optimized files

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app-header.css';
+const AppHeader = () => {
+    return (
+    <div className="app-header d-flex">
+        <h1>Ivan Komarnytskyi</h1>
+        <h2>5 posts, 3 liked</h2>
+    </div>
+    )
+}
+export default AppHeader;
